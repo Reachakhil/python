40c54f119c534c9d9be3337b1b11d8c90b02e14f
bhardwaj.akhil96@gmail.com <akhil.bhardwaj@accionlabs.com>
2018-09-26 16:52:19 +0530

    new

---
+++ 01data-structure.py
@@ -0,0 +1,8 @@
+with open('myfile.txt',mode ='r') as f:
+    print(f.read());
+#
+# with open('myfile.txt',mode='a') as f:
+#     f.write('iam akhil');
+
+
+
---
+++ a1.py
@@ -0,0 +1,66 @@
+from math import *
+fruit = "banana"
+bakedGood = " nut bread"
+print ("the value of this  is:",(fruit+bakedGood)*3)   #the comment
+print("hey \n guys".upper())
+print(type("hey guys"))
+print("59/60"+"kooo")
+z="59/60+kooo"
+print(len(z))
+print(z[0])
+print(z.index("k"))
+print(z.replace("koo","hooo"))
+print(z.split('+'))
+
+#print("enter your name")
+#l=input()
+#print("hey "+l)
+x=5
+print(str(x)+"my favourite number");
+print(sqrt(9))
+# x=input("enter yo name")
+# print("name is "+ x)
+# x=int(input("first"))
+# y=int(input("second"))
+# z=x+y
+# print(z);
+print(8//6)
+print(8%6)
+print(6**2)
+print(9**0.5)
+print((2+10)*(10*3))
+a=5
+print(a+a)
+a=a+a
+print(a)
+my_income=100
+tax_rate=0.33
+my_taxes=my_income*tax_rate
+print("hello my name is \"akhil\"");
+print(len("my name is \n akhil"))
+
+s='hello world'
+print(s[4])
+print(s[0:])  # zero till end
+print(s[:5])  # zero till 5th character
+print(s.split())
+
+s='akhil'
+print("my name is %s" %s) #Another way for string
+
+
+my_list=[0,1,'akhil', 'hey akhil'];
+print(len(my_list))
+print(my_list[:2])
+my_list=my_list+['ashok'] #concat something to list
+print(my_list[0:])
+my_list.append('abdu')
+print(my_list)
+my_list.pop()
+print(my_list)
+z=my_list.pop()
+print(z)
+print(my_list)
+
+
+
